89cb3cde594169359726add458f658632e69ea10
Roberto Cabrales <Roberto Cabrales>
2019-02-28 09:56:02 -0800

    Initial commit

---
+++ src/main/java/com/edu/appswbd/practica/dos/curriculum/CurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.edu.appswbd.practica.dos.curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurriculumApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/edu/appswbd/practica/dos/curriculum/model/CurriculumVitae.java
@@ -0,0 +1,20 @@
+package com.edu.appswbd.practica.dos.curriculum.model;
+
+import lombok.Data;
+
+@Data
+public class CurriculumVitae {
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String direccion;
+    private String correo;
+    private String telefono;
+    private String universidad;
+    private String carrera;
+    private String preparatoria;
+    private String capacitacion;
+    private String idiomaNat;
+    private String idiomas;
+}
+
