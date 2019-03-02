de23149f3f109fd3e70633d2cb09fa6ab2fee2ef
roberto.cabrales@uabc.edu.mx <Paparazzi3107>
2019-03-02 00:04:54 -0800

    subiendo

--- src/main/java/com/edu/appswbd/practica/dos/curriculum/CurriculumApplication.java
+++ src/main/java/com/edu/appswbd/practica/dos/curriculum/CurriculumApplication.java
@@ -2,12 +2,35 @@ package com.edu.appswbd.practica.dos.curriculum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class CurriculumApplication {
+public class CurriculumApplication extends WebMvcConfigurerAdapter {
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         SpringApplication.run(CurriculumApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
